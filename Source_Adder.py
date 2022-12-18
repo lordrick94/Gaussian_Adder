7087e74a7b37907c6a8f7ba6c262888bdea18dc1
Lordrick <lordrick94@gmail.com>
2022-12-18 17:31:56 +0300

    Second Commit with major modifications - Code is now running better

--- Source_Adder.py
+++ Source_Adder.py
@@ -1,11 +1,6 @@
 """
 This Code can be Used to add fake sources to a Data Cube
 This is the First Draft therefore Still many Bugs to Fix
-Known Bugs:
-    1. Defining the Std of the Gaussians in all 3 dimensions
-    2. The Sources are identical
-    3. When Sources are defined out of Range
-    4. Defining Positions in WCS
 
 But it Runs, Yay!!!!!!!
 """
@@ -16,16 +11,20 @@ import numpy as np
 from astropy.io import fits
 from astropy.coordinates import Angle
 from astropy import units as u
+import pandas as pd
 
-# Importing Source Generator
-from gaus_generator import gaussian_source
-
+# Importing source 
+from gaus_generator import Gaussian_Generator
 
 #Loading Fits File
 filename = input('Enter the File Name:')
 hdul = fits.open(filename)
 hdr = hdul[0].header                        #Header
 data = hdul[0].data[0]                      #Data
+
+#Taking shape of data_cube
+sh = data.shape
+t_x,t_y,t_z = sh[1],sh[2],sh[0]
 
 
 #Calculating Beam to Pixel Ratio
@@ -41,41 +40,36 @@ dmIN = Angle(hdr['BMIN'],u.degree)          #BMIN FWHM from header
 c_2 = np.abs(cdel_1.arcsec)
 d_2 = dmIN.arcsec
 
-t_x = d_1/c_1;t_y = d_2/c_2
+b_x = int(d_1/c_1);b_y = int(d_2/c_2)
+
+v = hdr['BPA']/70139.80849035224
+bpa = Angle(v,u.radian)
 
 # Number of Spanning Channells
 r = input('Enter the Number Channels to Span: ')
-r = int(r)
+b_z = int(r)
 
-#Generating 3D Gaussian Source
-gs,x_l,y_l,mu_z = gaussian_source(data,t_x,t_y,r)
+#Number of sources to be added
+n  = input('Enter the Number of Sources: ')
+ns  = int(n)
 
-# Entering the Number of Sources and their positions
-ns = input('Enter the Number of Sources:')
-ns = int(ns)
-sources = []
-for i in range(ns):
-    x_1 = input('Enter x value of Source {}: '.format(i+1))
-    y_1 = input('Enter y value of Source {}: '.format(i+1))
-    z_1 = input('Enter z value of Source {}: '.format(i+1))
-    x_1 = float(x_1)
-    y_1 = float(y_1)
-    z_1 = float(z_1)
-    pos = (x_1,y_1,z_1)
-    sources.append(pos)
+#Genrating source positions and Amplitudes
+a = 2*np.max(data)
+Amp = np.random.uniform(a,a/4,ns)
+s_x = np.random.randint(0,t_x,ns)
+s_y = np.random.randint(0,t_y,ns)
+s_z = np.random.randint(0,t_z,ns)
 
-#Adding sources to the Cube
-for source in sources:
-    x,y,z = source
-    y_1 = int(y)
-    y_2 = int(y+y_l)
-    x_1 = int(x)
-    x_2 = int(x+x_l)
-    z_1 = int(z-mu_z)
-    z_2 = int(z+mu_z)
-    arr = data[z_1:z_2,x_1:x_2,y_1:y_2]
-    arr+=gs
+df = pd.DataFrame(list(zip(s_x, s_y, s_z, Amp)),columns =['x_mean', 'y_mean', 'z_mean', 'Amplitude'])
+df.to_csv('source_data.csv')
+print(df)
 
+#Adding the Sources to data_cube
+for (x_m, y_m, z_m, amp) in zip(s_x, s_y, s_z, Amp):
+    gs = Gaussian_Generator(t_x,t_y,t_z,x_m,y_m,z_m,b_x,b_y,b_z,amp,bpa)
+    hdul[0].data[0] = hdul[0].data[0]+gs
 
-hdul.writeto('sources_'+filename,overwrite=True)
+#Saving to new fits file
+hdul.writeto('sources + '+filename,overwrite=True)
 
+print('{} sources have been added to the {} data cube'.format(n,filename))
--- gaus_generator.py
+++ gaus_generator.py
@@ -1,15 +1,12 @@
 import numpy as np
-def gaussian_source(data,t_x,t_y,r):
-    mu_x = t_x/2
-    mu_y = t_y/2
-    mu_z = r/2
-    b_x = t_x/300
-    b_y = t_y/200                               # b values need work
-    b_z = r/640
-    x,y,z = np.meshgrid(np.linspace(0,t_x,int(t_x)),np.linspace(0,t_y,int(t_y)),np.linspace(0,r,r))
-    Amp = 4*np.max(data)
-    gs = Amp*np.exp(-(((x-mu_x)**2)/2*b_x**2+((y-mu_y)**2)/2*b_y**2)+((z-mu_z)**2/2*b_z**2))
-    gs = np.transpose(gs,(2,1,0))
-    x_l = gs.shape[1]
-    y_l = gs.shape[2]
-    return gs,x_l,y_l,mu_z
+from astropy.modeling.models import Gaussian2D as g2d, Gaussian1D as g1d
+
+
+def Gaussian_Generator(t_x,t_y,t_z,x_m,y_m,z_m,b_x,b_y,b_z,amp,bpa):
+    z,y,x = np.ogrid[0:t_z,0:t_y,0:t_x]
+    g_2 = g2d(amplitude=amp, x_mean=x_m, y_mean=y_m, x_stddev=b_x, y_stddev=b_y, theta=bpa) # Generating 2d gaussian psf
+    g_xy = g_2(x,y)
+    g_1 = g1d(amplitude=1, mean=z_m, stddev=b_z) # generating 1d gaussian in the spectral direction
+    g_z = g_1(z)
+    gs = g_xy*g_z
+    return gs
